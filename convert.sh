9bb9fbe7e666a710c39dc26501f6baef973552a1
Cooper <45991010+glowbase@users.noreply.github.com>
2024-11-13 15:58:32 +1100

    Add files via upload

--- convert.sh
+++ convert.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 
-# Input log file
-input_file=$1
-# Output log file
-output_file="auth_converted.log"
+LOG_FILE="$1"
+HEADERS="Date/Time,IP Address,HTTP Method,URI,Status Code,Bytes,Referrer,User Agent"
 
-# Read the input file line by line
+log_lines=$(cat "$LOG_FILE")
+lines="$HEADERS\n"
+
 while IFS= read -r line; do
-    # Extract date and time using grep
-    if echo "$line" | grep -qE '^[A-Za-z]{3} [ ]*[0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2}'; then
-        date_part=$(echo "$line" | awk '{print $1, $2}')
-        time_part=$(echo "$line" | awk '{print $3}')
+    ip=$(echo "$line" | cut -d " " -f 1)
+    time=$(echo "$line" | cut -d " " -f 4-5 | sed  's/\[//g' | sed 's/\]//g')
+    method=$(echo "$line" | cut -d " " -f 6 | sed 's/"//g')
+    uri=$(echo "$line" | cut -d " " -f 7)
+    code=$(echo "$line" | cut -d " " -f 9)
+    bytes=$(echo "$line" | cut -d " " -f 10)
+    referrer=$(echo "$line" | cut -d " " -f 11)
+    user_agent=$(echo "$line" | cut -d " " -f 12-20)
 
-        # Combine date and time for conversion
-        datetime="$date_part $time_part"
+    month=$(echo "$time" | sed -E 's/(Jan)/01/; s/(Feb)/02/; s/(Mar)/03/; s/(Apr)/04/; s/(May)/05/; s/(Jun)/06/; s/(Jul)/07/; s/(Aug)/08/; s/(Sep)/09/; s/(Oct)/10/; s/(Nov)/11/; s/(Dec)/12/')
+    
+    formatted_date=$(echo "$month" | cut -d ":" -f 1 | sed -E 's#([0-9]{2})/([0-9]{2})/([0-9]{4})#\3-\2-\1#')
+    formatted_time=$(echo "$time" | cut -d ":" -f 2-5)
+    utc_time=$(date -d "$formatted_date $formatted_time" -u +"%Y-%m-%dT%H:%M:%SZ")
 
-        # Convert to UTC (subtract 10 hours)
-        utc_time=$(date -d "$datetime AEST -10 hours" +"%b %d %H:%M:%S")
+    lines+="$utc_time,$ip,$method,$uri,$code,$bytes,$referrer,$user_agent\n"
+done <<< "$log_lines"
 
-        # Replace the original date and time with the converted one
-        converted_line="${line//$date_part $time_part/$utc_time}"
-        
-        # Output the converted line to the output file
-        echo "$converted_line" >> "$output_file"
-    else
-        # If the line doesn't match, just write it as is
-        echo "$line" >> "$output_file"
-    fi
-done < "$input_file"
-
-echo "Conversion complete. Output saved to $output_file."
+echo -e "$lines" >> "$LOG_FILE.csv"
